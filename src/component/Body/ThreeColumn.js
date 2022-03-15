964e4d25e3cd99241657316a2dd229477ad5cee1
Aayushojha <18egjcs024@gitjaipur.com>
2022-03-15 10:44:00 +0530

    some more bug fixed

--- src/component/Body/ThreeColumn.js
+++ src/component/Body/ThreeColumn.js
@@ -1,56 +1,60 @@
-import React from 'react'
-import axios from "axios"
-import ReactMarkdown from "react-markdown"
-import Style from "./../../modules/Body/Column.module.css"
-import root from "../../modules/responsive.module.css"
-import Card from "./Card"
+import React from "react";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import Style from "./../../modules/Body/Column.module.css";
+import root from "../../modules/responsive.module.css";
+import Card from "./Card";
 
-export default function ThreeColumn({id}) {
-    const baseURL = "http://localhost:1337/api/hey-himalayas/1?populate[Content][populate][Cards][populate]=*"
-    const [post, setPost] = React.useState(null);
-    // console.log("Two",id);
+export default function ThreeColumn({ id }) {
+  const baseURL =
+    "http://localhost:1337/api/hey-himalayas/1?populate[Content][populate][Cards][populate]=*";
+  const [post, setPost] = React.useState(null);
+  // console.log("Two",id);
 
-    React.useEffect(() => {
-      axios.get(baseURL).then((response) => {
+  React.useEffect(() => {
+    axios
+      .get(baseURL)
+      .then((response) => {
         // console.log("Three");
         // console.log(JSON.stringify(response.data.data.attributes.Content[id]));
         // console.log(JSON.stringify(response.data.data.attributes.Content[id].Variant));
         setPost(response.data.data.attributes.Content[id]);
         // console.log(Object.keys(response.data.data.attributes));
-      }).catch((error ) => {
+      })
+      .catch((error) => {
         console.log(error);
-      }) ;
-    }, []);
-  
-    if (!post) return null;
-    // console.log(post.Cards);
-    // console.log("------");
+      });
+  }, []);
+
+  if (!post) return null;
+  // console.log(post.Cards);
+  // console.log("------");
   return (
-    <div className={`${Style.row} ${root.md_col_11} ${root.sm_col_10} ${root.tb_col_10} ${root.col_10}  ${root.l_col_11}`}>
-        <div className={Style.rowTitle}><h2>Best Things to do in Dubai in May</h2></div>
-        {/* <div className={Style.rowDesc}></div> */}
-        <div className={Style.columnContainer}>
-            {
-                post.Cards.map((value , key) => {
-                    return (
-                        <div className={Style.ThreeColumn} key= { key }>
-                            <Card type="threeColumn" value = { value } />
-                        </div>
-                    )
-                })
-            }
-            
-           
-        </div>
+    <div
+      className={`${Style.row} ${root.md_col_11} ${root.sm_col_10} ${root.tb_col_10} ${root.col_10}  ${root.l_col_11}`}
+    >
+      <div className={Style.rowTitle}>
+        <ReactMarkdown>{post.Heading}</ReactMarkdown>
+      </div>
+      {/* <div className={Style.rowDesc}></div> */}
+      <div className={Style.columnContainer}>
+        {post.Cards.map((value, key) => {
+          return (
+            <div className={Style.ThreeColumn} key={key}>
+              <Card type="threeColumn" value={value} />
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export function ThreeColumnContent({ data }){
-    return(
-        <div className={Style.ThreeColumnContentContainer}>
-            <ReactMarkdown>{data}</ReactMarkdown>
-            {/* <div className={Style.ThreeTitle}>
+export function ThreeColumnContent({ data }) {
+  return (
+    <div className={Style.ThreeColumnContentContainer}>
+      <ReactMarkdown>{data}</ReactMarkdown>
+      {/* <div className={Style.ThreeTitle}>
                 <h3>Why You Should Visit </h3>
             </div>
             <div className={Style.ThreeDesc}>
@@ -63,6 +67,6 @@ export function ThreeColumnContent({ data }){
                 <b>May Timings:</b> 7 AM to 7 PM
                 <br/><br/>
             </div> */}
-        </div>
-    )
+    </div>
+  );
 }
