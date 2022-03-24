dcbb060dd76da296781afc78c35904c0e034c76a
Aayushojha <18egjcs024@gitjaipur.com>
2022-03-24 18:35:08 +0530

    dynamic routing with only slug

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import Footer from './component/Footer';
 import Product from './component/Product';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PageBody from './component/PageBody';
-import Banner from './component/Body/Banner';
+import PageRenderer from './component/PageRenderer';
 
 function App() {
   return (
     <div className="container">
       <Router>
         <NavBar />
-        <Banner />
         <Routes className="center">
           <Route path="/" exact element={<Body />} />
-          <Route path="/:routeName/:pageId" exact element={<PageBody />} />
+          {
+            //       <Route path="/:routeName/:pageId" exact element={<PageBody />} />
+          }
+          <Route path="/:routeName" exact element={<PageRenderer />} />
           <Route path="/product/:productId" element={<Product />} />
           <Route path="*" element={<h1>404 PAGE NOT FOUND</h1>} />
         </Routes>
